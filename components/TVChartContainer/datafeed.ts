63325903b89bbea8c47b4972941b7110cdc6c1ad
av-equidity <158487452+av-equidity@users.noreply.github.com>
2024-02-12 09:45:52 +0530

    Update datafeed.ts

--- components/TVChartContainer/datafeed.ts
+++ components/TVChartContainer/datafeed.ts
@@ -137,7 +137,6 @@ export default {
             subscriberUID,
             onResetCacheNeededCallback,
             toTimeStamp)
-        );
     },
 
     unsubscribeBars: (subscriberUID:any) => {
